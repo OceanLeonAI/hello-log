b3aa2333f74e6e4dcbbdb2e09c10d51d0d63b46f
OceanLeonAI <beyondjayfir@126.com>
2021-08-04 23:05:36 +0800

    添加相关配置

--- log-jul/src/test/java/com/leon/JULTest.java
+++ log-jul/src/test/java/com/leon/JULTest.java
@@ -2,6 +2,7 @@ package com.leon;
 
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.util.logging.*;
 
 /**
@@ -59,7 +60,7 @@ public class JULTest {
         logger.setLevel(Level.ALL);
         consoleHandler.setLevel(Level.ALL);
         // 二、输出到日志文件
-        FileHandler fileHandler = new FileHandler("F:\\jul.log");
+        FileHandler fileHandler = new FileHandler("E:\\jul.log");
         fileHandler.setFormatter(simpleFormatter);
         logger.addHandler(fileHandler);
         // 2.日志记录输出
@@ -71,4 +72,57 @@ public class JULTest {
         logger.finer("finer");
         logger.finest("finest");
     }
+
+    @Test
+    public void testLogParent() throws Exception {
+        // 日志记录器对象父子关系
+        Logger logger1 = Logger.getLogger("com.leon.JULTest");
+        Logger logger2 = Logger.getLogger("com.leon");
+        System.out.println(logger1.getParent() == logger2);
+        // 所有日志记录器对象的顶级父元素 class为java.util.logging.LogManager$RootLogger name为 ""
+        System.out.println("logger2 parent:" + logger2.getParent() + "，name：" +
+                logger2.getParent().getName());
+        // 一、自定义日志级别
+        // a.关闭系统默认配置
+        logger2.setUseParentHandlers(false);
+        // b.创建handler对象
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        // c.创建formatter对象
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+        // d.进行关联
+        consoleHandler.setFormatter(simpleFormatter);
+        logger2.addHandler(consoleHandler);
+        // e.设置日志级别
+        logger2.setLevel(Level.ALL);
+        consoleHandler.setLevel(Level.ALL);
+        // 测试日志记录器对象父子关系
+        logger1.severe("severe");
+        logger1.warning("warning");
+        logger1.info("info");
+        logger1.config("config");
+        logger1.fine("fine");
+        logger1.finer("finer");
+        logger1.finest("finest");
+    }
+
+    @Test
+    public void testProperties() throws Exception {
+        // 读取自定义配置文件
+        InputStream in =
+                JULTest.class.getClassLoader().getResourceAsStream("logging.properties");
+        // 获取日志管理器对象
+        LogManager logManager = LogManager.getLogManager();
+        // 通过日志管理器加载配置文件
+        logManager.readConfiguration(in);
+        Logger logger = Logger.getLogger("com.leon.JULTest");
+        logger.severe("severe");
+        logger.warning("warning");
+        logger.info("info");
+        logger.config("config");
+        logger.fine("fine");
+        logger.finer("finer");
+        logger.finest("finest");
+    }
+
+
 }
