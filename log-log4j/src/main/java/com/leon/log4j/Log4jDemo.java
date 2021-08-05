9f93e44385c11b6ea10f26272024a2a36324f2ab
OceanLeonAI <beyondjayfir@126.com>
2021-08-05 16:51:10 +0800

    log4j 日志案例 修改

--- log-log4j/src/main/java/com/leon/log4j/Log4jDemo.java
+++ log-log4j/src/main/java/com/leon/log4j/Log4jDemo.java
@@ -1,4 +1,7 @@
 package com.leon.log4j;
 
 public class Log4jDemo {
+    public static void main(String[] args) {
+        System.out.println("hello Log4jDemo");
+    }
 }
--- log-log4j/src/test/java/com/leon/Log4jTest.java
+++ log-log4j/src/test/java/com/leon/Log4jTest.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 
 public class Log4jTest {
 
+    /**
+     * 快速入门
+     *
+     * @throws Exception
+     */
     @Test
     public void testQuick() throws Exception {
         // 初始化系统配置，不需要配置文件
@@ -21,6 +26,37 @@ public class Log4jTest {
         logger.info("info"); // 程序运行信息，数据库的连接、网络、IO操作等
         logger.debug("debug"); // 调试信息，一般在开发阶段使用，记录程序的变量、参数等
         logger.trace("trace"); // 追踪信息，记录程序的所有流程信息
+    }
+
+    /**
+     * 自定义 Logger
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testCustomLogger() throws Exception {
+        // 初始化系统配置，不需要配置文件
+        BasicConfigurator.configure();
+
+        // 自定义 com.itheima
+        Logger logger1 = Logger.getLogger(Log4jTest.class);
+        logger1.fatal("fatal"); // 严重错误，一般会造成系统崩溃和终止运行
+        logger1.error("error"); // 错误信息，但不会影响系统运行
+        logger1.warn("warn"); // 警告信息，可能会发生问题
+        logger1.info("info"); // 程序运行信息，数据库的连接、网络、IO操作等
+        logger1.debug("debug"); // 调试信息，一般在开发阶段使用，记录程序的变量、参数等
+        logger1.trace("trace"); // 追踪信息，记录程序的所有流程信息
+
+        System.out.println("================================");
+
+        // 自定义 org.apache
+        Logger logger2 = Logger.getLogger(Logger.class);
+        logger2.fatal("fatal logger2"); // 严重错误，一般会造成系统崩溃和终止运行
+        logger2.error("error logger2"); // 错误信息，但不会影响系统运行
+        logger2.warn("warn logger2"); // 警告信息，可能会发生问题
+        logger2.info("info logger2"); // 程序运行信息，数据库的连接、网络、IO操作等
+        logger2.debug("debug logger2"); // 调试信息，一般在开发阶段使用，记录程序的变量、参数等
+        logger2.trace("trace logger2"); // 追踪信息，记录程序的所有流程信息
     }
 }
 
