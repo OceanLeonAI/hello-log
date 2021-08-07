44a5dc2ba1df6215288bc2ff6e68d4ce0db390b0
OceanLeonAI <beyondjayfir@126.com>
2021-08-07 16:26:46 +0800

    log4j 日志框架 demo修改

--- log-log4j/src/test/java/com/leon/Log4jTest.java
+++ log-log4j/src/test/java/com/leon/Log4jTest.java
@@ -27,6 +27,7 @@ public class Log4jTest {
         // 日志记录输出
         logger.info("hello log4j");
 
+//        for (int i = 0; i < 1000; i++) {
         // 日志级别
         logger.fatal("fatal"); // 严重错误，一般会造成系统崩溃和终止运行
         logger.error("error"); // 错误信息，但不会影响系统运行
@@ -35,6 +36,16 @@ public class Log4jTest {
         logger.debug("debug"); // 调试信息，一般在开发阶段使用，记录程序的变量、参数等
 
         logger.trace("trace"); // 追踪信息，记录程序的所有流程信息
+//        }
+
+        // 再创建一个日志记录器对象
+        Logger logger1 = Logger.getLogger(Logger.class);
+        logger1.fatal("fatal logger1"); // 严重错误，一般会造成系统崩溃和终止运行
+        logger1.error("error logger1"); // 错误信息，但不会影响系统运行
+        logger1.warn("warn logger1"); // 警告信息，可能会发生问题
+        logger1.info("info logger1"); // 程序运行信息，数据库的连接、网络、IO操作等
+        logger1.debug("debug logger1"); // 调试信息，一般在开发阶段使用，记录程序的变量、参数等
+
     }
 
     /**
